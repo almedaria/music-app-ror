1b8d030a775c4e0d44eafd9658733f6b973dcbc3
almedaria <rmmgalmeda@gmail.com>
2021-11-19 20:26:58 +0800

    updated tracks table

--- db/migrate/20211119115028_create_tracks.rb
+++ db/migrate/20211119115028_create_tracks.rb
@@ -1,8 +1,12 @@
 class CreateTracks < ActiveRecord::Migration[6.1]
   def change
     create_table :tracks do |t|
-
       t.timestamps
+      t.text :URL
+      t.string :name
+      t.text :credits
+      t.boolean :available, default:false
+      t.datetime :published_at
     end
   end
 end
